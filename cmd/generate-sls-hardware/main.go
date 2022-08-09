2a4d3a8978f355dc6b36bea63cf4c49d17762eaf
Ryan Sjostrand <sjostrand@hpe.com>
2022-08-09 14:22:44 -0500

    Make generate-sls-hardware use the ccj package

--- cmd/generate-sls-hardware/main.go
+++ cmd/generate-sls-hardware/main.go
@@ -5,11 +5,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
-	"strings"
 
-	"github.com/Cray-HPE/cray-site-init/pkg/csi"
-	sls_common "github.com/Cray-HPE/hms-sls/pkg/sls-common"
-	"github.com/Cray-HPE/hms-xname/xnametypes"
 	"github.hpe.com/sjostrand/topology-tool/pkg/ccj"
 	"github.hpe.com/sjostrand/topology-tool/pkg/configs"
 	"gopkg.in/yaml.v2"
@@ -20,7 +16,7 @@ import (
 // TODO network hardware objects are missing IPs (which is a hold over)
 
 func main() {
-	if len(os.Args) != 4 {
+	if len(os.Args) != 3 {
 		panic("Incorrect number of CLI args provided")
 	}
 
@@ -42,20 +38,16 @@ func main() {
 	supportedArchitectures := map[string]bool{
 		"network_v2_tds": true,
 		"network_v2":     true,
+		"network_v1":     true,
 	}
 	if !supportedArchitectures[paddle.Architecture] {
 		err := fmt.Errorf("unsupported paddle architecture (%v)", paddle.Architecture)
 		panic(err)
 	}
 
-	// Read in cabinet lookup
-	cabinetLookupRaw, err := ioutil.ReadFile(os.Args[2])
+	// Determine the cabinet in cabinet lookup
+	cabinetLookup, err := ccj.DetermineCabinetLookup(paddle)
 	if err != nil {
-		panic(err)
-	}
-
-	var cabinetLookup configs.CabinetLookup
-	if err := yaml.Unmarshal(cabinetLookupRaw, &cabinetLookup); err != nil {
 		panic(err)
 	}
 
@@ -70,123 +62,20 @@ func main() {
 		fmt.Println(string(jsonRaw))
 	}
 
-	// Read in application_node_config.yaml
-	// TODO the prefixes list is not being used, as we are assuming all unknown nodes are application
-	applicationNodeRaw, err := ioutil.ReadFile(os.Args[3])
+	// Read in application_node_metadata.yaml
+	applicationNodeMetadataRaw, err := ioutil.ReadFile(os.Args[2])
 	if err != nil {
 		panic(err)
 	}
 
-	var applicationNodeConfig csi.SLSGeneratorApplicationNodeConfig
-	if err := yaml.Unmarshal(applicationNodeRaw, &applicationNodeConfig); err != nil {
-		panic(err)
-	}
-	if err := applicationNodeConfig.Normalize(); err != nil {
-		panic(err)
-	}
-	if err := applicationNodeConfig.Validate(); err != nil {
+	var applicationNodeMetadata configs.ApplicationNodeMetadataMap
+	if err := yaml.Unmarshal(applicationNodeMetadataRaw, &applicationNodeMetadata); err != nil {
 		panic(err)
 	}
 
-	// Iterate over the paddle file to build of SLS data
-	allHardware := map[string]sls_common.GenericHardware{}
-	for _, topologyNode := range paddle.Topology {
-		fmt.Println(topologyNode.Architecture, topologyNode.CommonName)
-
-		//
-		// Build the SLS hardware representation
-		//
-		hardware, err := ccj.BuildSLSHardware(topologyNode, paddle, cabinetLookup, applicationNodeConfig)
-		if err != nil {
-			panic(err)
-		}
-
-		// Ignore empty hardware
-		if hardware.Xname == "" {
-			continue
-		}
-
-		// Verify cabinet exists (ignore CDUs)
-		if strings.HasPrefix(hardware.Xname, "x") {
-			cabinetXname, err := csi.CabinetForXname(hardware.Xname)
-			if err != nil {
-				panic(err)
-			}
-
-			if !cabinetLookup.CabinetExists(cabinetXname) {
-				err := fmt.Errorf("unknown cabinet (%s)", cabinetXname)
-				panic(err)
-			}
-		}
-
-		// Verify new hardware
-		if _, present := allHardware[hardware.Xname]; present {
-			err := fmt.Errorf("found duplicate xname %v", hardware.Xname)
-			panic(err)
-		}
-
-		allHardware[hardware.Xname] = hardware
-
-		//
-		// Build up derived hardware
-		//
-		if hardware.TypeString == xnametypes.ChassisBMC {
-			allHardware[hardware.Xname] = sls_common.NewGenericHardware(hardware.Parent, hardware.Class, nil)
-		}
-
-		//
-		// Build the MgmtSwitchConnector for the hardware
-		//
-
-		mgmtSwtichConnector, err := ccj.BuildSLSMgmtSwitchConnector(hardware, topologyNode, paddle)
-		if err != nil {
-			panic(err)
-		}
-
-		// Ignore empty mgmtSwtichConnectors
-		if mgmtSwtichConnector.Xname == "" {
-			continue
-		}
-
-		if _, present := allHardware[mgmtSwtichConnector.Xname]; present {
-			err := fmt.Errorf("found duplicate xname %v", mgmtSwtichConnector.Xname)
-			panic(err)
-		}
-
-		allHardware[mgmtSwtichConnector.Xname] = mgmtSwtichConnector
-	}
-
-	// Generate Cabinet Objects
-	for cabinetKind, cabinets := range cabinetLookup {
-		for _, cabinet := range cabinets {
-			class, err := cabinetKind.Class()
-			if err != nil {
-				panic(err)
-			}
-
-			extraProperties := sls_common.ComptypeCabinet{
-				Networks: map[string]map[string]sls_common.CabinetNetworks{}, // TODO this should be outright removed. MEDS and KEA no longer look here
-			}
-
-			if cabinetKind.IsModel() {
-				extraProperties.Model = string(cabinetKind)
-			}
-
-			hardware := sls_common.NewGenericHardware(cabinet, class, extraProperties)
-
-			// Verify new hardware
-			if _, present := allHardware[hardware.Xname]; present {
-				err := fmt.Errorf("found duplicate xname %v", hardware.Xname)
-				panic(err)
-			}
-
-			allHardware[hardware.Xname] = hardware
-		}
-	}
-
-	// Build up and the SLS state
-	slsState := sls_common.SLSState{
-		Hardware: allHardware,
+	slsState, err := ccj.BuildExpectedHardwareState(paddle, cabinetLookup, applicationNodeMetadata)
+	if err != nil {
+		panic(err)
 	}
 
 	// Write out the SLS State file
